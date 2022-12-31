95dc488901ff95ed8b135f0e43387bb1245fcb3b
mohsen akbari <malevin.git@gmail.com>
2022-12-31 14:32:01 +0330

    add GetWeather in api

--- core/todo/api/v1/urls.py
+++ core/todo/api/v1/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'task', views.TaskModelViewSet)
+router.register(r'weather',views.GetWeather,basename="weather")
 
 app_name = "api-v1"
 urlpatterns = router.urls
--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -1,21 +1,21 @@
-from rest_framework.permissions import (
-    IsAuthenticated,
-)
-from rest_framework.response import Response
+# from rest_framework.decorators import action
+# from rest_framework.filters import SearchFilter, OrderingFilter
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import viewsets
-
-from rest_framework.decorators import action
-from rest_framework.filters import SearchFilter, OrderingFilter
-
+from django.views.decorators.vary import vary_on_cookie
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from todo.api.v1.serializers import TaskSerializer
 from todo.models import Task
+import requests
 
 
 class TaskModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
-    
+
     def get_queryset(self):
         return self.queryset.filter(author=self.request.user)
     # filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
@@ -25,6 +25,16 @@ class TaskModelViewSet(viewsets.ModelViewSet):
     # ordering_fields = ["published_date"]
     # pagination_class = DefaultPagination
 
+
+class GetWeather(viewsets.ViewSet):
+    # With cookie: cache requested url for each user for 2 hours
+    @method_decorator(cache_page(20*60))
+    @method_decorator(vary_on_cookie)
+    def list(self, request, format=None):
+        API_key = "5ad0e4cf5c414c0c903104202223112"
+        url = f"http://api.weatherapi.com/v1/current.json?key={API_key}&q=32.661343,51.680374&aqi=yes"
+        content = requests.get(url).json()
+        return Response(content)
 
 # class CategoryModelViewSet(viewsets.ModelViewSet):
 #     permission_classes = [IsAuthenticated]
